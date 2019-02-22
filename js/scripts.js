3e7ec53073d00feb3f80b9ad78355a7b90e644ca
jasonmuchiri <jasonmkinyua@gmail.com>
2019-02-22 18:01:08 +0300

    changes in js

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,7 @@ $(document).ready(function(){
   var two = parseInt($("input:radio[name=two]:checked").val());
   var three = parseInt($("input:radio[name=three]:checked").val());
   var four = parseInt($("input:radio[name=four]:checked").val());
+  var score = (one + two + three + four)
+  $("#display").text(((score)/40)*100)
  });
 });
