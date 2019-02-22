11e713aa802416782f722fa0029a8ed3bcb3d9e4
jasonmuchiri <jasonmkinyua@gmail.com>
2019-02-22 17:53:42 +0300

    changes in js

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,6 @@ $(document).ready(function(){
   var one = parseInt($("input:radio[name=one]:checked").val());
   var two = parseInt($("input:radio[name=two]:checked").val());
   var three = parseInt($("input:radio[name=three]:checked").val());
+  var four = parseInt($("input:radio[name=four]:checked").val());
  });
 });
