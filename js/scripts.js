12124a6949b94b4516db419d493decf4801df366
jasonmuchiri <jasonmkinyua@gmail.com>
2019-02-22 17:18:24 +0300

    changes in js

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
  $("#quiz").submit(function(event){
   event.preventDefault();
   var one = parseInt($("input:radio[name=one]:checked").val());
+  var two = parseInt($("input:radio[name=two]:checked").val());
  });
 });
